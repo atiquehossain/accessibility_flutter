28f4453acc04f72d6afc93261ac9aeef8bbf68e4
atique.hossain <atique.hossain@savethechildren.org>
2024-08-18 13:51:45 +0600

    backup

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,3 +1,5 @@
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:android_intent_plus/flag.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -10,6 +12,32 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(child:
+    Scaffold(
+      body: Column(
+        children: [
+          Semantics(
+            excludeSemantics: true,
+            hint: '2 minus bracket 3 over 5',
+            child: const Text('2 - (3/5)'),
+          ),
+      Center(
+        child: ElevatedButton(
+          onPressed: _openAccessibilitySettings,
+          child: const Text('Open Accessibility Settings'),
+        ),
+      ),
+
+        ],
+      ),
+    ));
+  }
+
+  void _openAccessibilitySettings() {
+    const intent = AndroidIntent(
+      action: 'android.settings.ACCESSIBILITY_SETTINGS',
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    intent.launch();
   }
 }
